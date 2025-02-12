f061280dc39e4ce836e03b7c8bc8aaef74fec353
emillantz <emil.lantz@gmail.com>
2025-02-12 20:49:09 +0100

    multi-lamp

--- lib/timers/timers.cpp
+++ lib/timers/timers.cpp
@@ -6,11 +6,14 @@
 void setup_tc3() {
     TC3->COUNT16.INTENSET.bit.OVF = 1;
     TC3->COUNT16.INTENSET.bit.ERR = 1;
+
     
     // Configure asynchronous clock source
+
+
     GCLK->CLKCTRL.reg = GCLK_CLKCTRL_ID_TCC2_TC3_Val;    // select TC3 peripheral channel
-    GCLK->CLKCTRL.reg |= GCLK_CLKCTRL_GEN_GCLK0;        // select source GCLK_GEN[0]
-    GCLK->CLKCTRL.bit.CLKEN = 1;            // enable TC3 generic clock
+    GCLK->CLKCTRL.reg |= GCLK_CLKCTRL_GEN_GCLK0;        // select source GCLK_GEN[0] (1MHz ??)
+    GCLK->CLKCTRL.bit.CLKEN = 1;                        // enable the clock
     // Configure synchronous bus clock
     PM->APBCSEL.bit.APBCDIV = 0;            // no prescaler
     PM->APBCMASK.bit.TC3_ = 1;                // enable TC3 interface
@@ -41,36 +44,42 @@ void setup_tc3() {
 }
 
 void setup_tc4() {
-    //TC4->COUNT16.INTENSET.bit.OVF = 1;
-    //TC4->COUNT16.INTENSET.bit.ERR = 1;
-    //
-    //
-    //GCLK->CLKCTRL.reg = GCLK_CLKCTRL_ID_TC
-    //// Configure synchronous bus clock
-    //PM->APBCSEL.bit.APBCDIV = 0;            // no prescaler
-    //PM->APBCMASK.bit.TC4_ = 1;                // enable TC4 interface
-    //
-    //// Configure Count Mode (16-bit)
-    //TC4->COUNT16.CTRLA.bit.MODE = 0x0;
-    //
-    //// Configure Prescaler for divide by 8 (500kHz clock to COUNT)
-    //TC4->COUNT16.CTRLA.bit.PRESCALER = 0x3;
-    //
-    //// Configure TC3 Compare Mode for compare channel 0
-    //TC4->COUNT16.CTRLA.bit.WAVEGEN = 0x1;            // "Match Frequency" operation
-    //
-    //// Initialize compare value for x s @ 500kHz (reg = x * 500k)
-    //TC4->COUNT16.CC[0].reg = 50000;
-    //
-    //// Enable TC3 compare mode interrupt generation
-    //TC4->COUNT16.INTENSET.bit.MC0 = 0x1;    // Enable match interrupts on compare channel 0 
-    //
-    //// Enable TC3
-    //TC4->COUNT16.CTRLA.bit.ENABLE = 1;
-    //
-    //// Wait until TC3 is enabled
-    //while(TC4->COUNT16.STATUS.bit.SYNCBUSY == 1);
-    //
-    //NVIC_SetPriority(TC4_IRQn, 3);
-    //NVIC_EnableIRQ(TC4_IRQn);
+    TC4->COUNT16.INTENSET.bit.OVF = 1;
+    TC4->COUNT16.INTENSET.bit.ERR = 1;
+    
+    GCLK->GENCTRL.reg = GCLK_GENCTRL_ID(2);    // select GCLK_GEN[2]
+    GCLK->GENCTRL.reg |= GCLK_GENCTRL_SRC_OSC8M;    // select source OSC8M (8MHz)
+    GCLK->GENCTRL.reg |= GCLK_GENCTRL_IDC;    // enable improved duty cycle
+    GCLK->GENCTRL.reg |= GCLK_GENCTRL_GENEN;    // enable the clock
+
+    GCLK->CLKCTRL.reg = GCLK_CLKCTRL_ID_TC4_TC5_Val;
+    GCLK->CLKCTRL.reg |= GCLK_CLKCTRL_GEN_GCLK2;
+    GCLK->CLKCTRL.bit.CLKEN = 1;
+    // Configure synchronous bus clock
+    PM->APBCSEL.bit.APBCDIV = 0;            // no prescaler
+    PM->APBCMASK.bit.TC4_ = 1;                // enable TC4 interface
+    
+    // Configure Count Mode (16-bit)
+    TC4->COUNT16.CTRLA.bit.MODE = 0x0;
+    
+    // Configure Prescaler for divide by 8 (500kHz clock to COUNT)
+    TC4->COUNT16.CTRLA.bit.PRESCALER = 0x3;
+    
+    // Configure TC3 Compare Mode for compare channel 0
+    TC4->COUNT16.CTRLA.bit.WAVEGEN = 0x1;            // "Match Frequency" operation
+    
+    // Initialize compare value for x s @ 500kHz (reg = x * 500k)
+    TC4->COUNT16.CC[0].reg = 10000;
+    
+    // Enable TC3 compare mode interrupt generation
+    TC4->COUNT16.INTENSET.bit.MC0 = 0x1;    // Enable match interrupts on compare channel 0 
+    
+    // Enable TC3
+    TC4->COUNT16.CTRLA.bit.ENABLE = 1;
+    
+    // Wait until TC3 is enabled
+    while(TC4->COUNT16.STATUS.bit.SYNCBUSY == 1);
+    
+    NVIC_SetPriority(TC4_IRQn, 3);
+    NVIC_EnableIRQ(TC4_IRQn);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -10,37 +10,48 @@
 #endif
 #define PIN        5
 #define NUMPIXELS 12
-#define DELAYVAL 4
 
-volatile uint8_t t;
-volatile uint8_t tmp;
-uint8_t heartbeat_wf[256] = {
+volatile uint8_t t_3;
+volatile uint8_t t_4;
+volatile uint8_t tc3_val;
+volatile uint8_t tc4_val;
+
+volatile uint8_t heartbeat_wf[256] = {
 35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,38,41,43,46,48,51,53,55,57,58,59,59,59,59,59,59,58,58,57,56,55,54,52,50,48,47,46,44,42,41,40,38,37,35,35,35,35,37,38,39,40,40,39,36,35,35,35,35,35,35,35,35,52,76,103,130,157,182,205,224,238,248,254,255,251,242,229,212,191,167,140,113,86,61,39,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,36,37,37,38,39,41,42,44,46,48,50,51,53,56,58,61,63,65,67,70,72,75,77,79,82,84,86,88,90,92,94,96,98,100,101,103,103,104,103,103,102,101,100,98,95,92,89,86,82,78,73,68,64,59,55,50,45,41,37,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35
 };
 
-void set_pixels(uint8_t r, uint8_t g, uint8_t b);
+volatile void set_pixels_1(uint8_t r, uint8_t g, uint8_t b);
+volatile void set_pixels_2(uint8_t r, uint8_t g, uint8_t b);
 
-Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel tc1_slave(NUMPIXELS, 5, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel tc2_slave(NUMPIXELS, 4, NEO_GRB + NEO_KHZ800);
 
 void TC3_Handler() {
     TC3->COUNT16.INTFLAG.reg |= 0b00010000;
-    t++;
-    tmp = heartbeat_wf[t];
-    set_pixels(tmp, 0, 0);
+    t_3++;
+    tc3_val = heartbeat_wf[t_3];
+    set_pixels_1(tc3_val, 0, 0);
 }
 
 void TC4_Handler() {
     TC4->COUNT16.INTFLAG.reg |= 0b00010000;
-    t++;
-    tmp = heartbeat_wf[t];
-    set_pixels(0, tmp, 0);
+    t_4++;
+    tc4_val = heartbeat_wf[t_4];
+    set_pixels_2(tc4_val, 0, 0);
 }
 
-void set_pixels(uint8_t r, uint8_t g, uint8_t b) {
+volatile void set_pixels_1(uint8_t r, uint8_t g, uint8_t b) {
     for(int i=0; i<NUMPIXELS; i++) {
-        pixels.setPixelColor(i, pixels.Color(r, g, b));
+        tc1_slave.setPixelColor(i, tc1_slave.Color(r, g, b));
     }
-    pixels.show();
+    tc1_slave.show();
+}
+
+volatile void set_pixels_2(uint8_t r, uint8_t g, uint8_t b) {
+    for(int i=0; i<NUMPIXELS; i++) {
+        tc2_slave.setPixelColor(i, tc2_slave.Color(r, g, b));
+    }
+    tc2_slave.show();
 }
 
 
@@ -48,12 +59,14 @@ void setup() {
 #if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
     clock_prescale_set(clock_div_1);
 #endif
-    pixels.begin();
-    t = 0;
-    //setup_tcc1();
+    noInterrupts();
+    tc1_slave.begin();
+    tc2_slave.begin();
+    t_3 = 0;
+    t_4 = 0;
     setup_tc3();
-    //setup_tc4();
-
+    setup_tc4();
+    interrupts();
 }
 
 void loop() {
